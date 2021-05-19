7383e6661d74a1fa2d39e1f5b79e26b614c29190
Mikhail Kuleshov <kuleshov322@gmail.com>
2021-05-19 12:49:19 +0300

    2_1_2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean1= applicationContext.getBean("helloworld",HelloWorld.class);
+
+        System.out.println(bean == bean1);
+
+        Cat catBean = applicationContext.getBean("cat",Cat.class);
+        Cat catBean1 = applicationContext.getBean("cat",Cat.class);
+        System.out.println(catBean1 == catBean);
         System.out.println(bean.getMessage());
     }
 }
